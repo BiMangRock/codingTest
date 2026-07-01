e835996b49f5f707dc437c307589ecaa4a6671fa
BiMangRock <changminytur2132@naver.com>
2026-07-01 09:44:20 +0900

    뒤에 있는 가장 가까운 같은 글자

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -5,45 +5,39 @@ public class Main {
         // 테스트 케이스 1
         String s1 = "banana";
         int[] result1 = solution(s1);
-        System.out.println("테스트 케이스 1 결과: " + Arrays.toString(result1)); // 예상 출력: [-1, -1, -1, 2, 2, 2]
+        System.out.println("테스트 케이스 1 결과: " + Arrays.toString(result1)); // 예상 출력: [-1, 2, 2, 2, -1, -1]
 
         // 테스트 케이스 2
         String s2 = "foobar";
         int[] result2 = solution(s2);
-        System.out.println("테스트 케이스 2 결과: " + Arrays.toString(result2)); // 예상 출력: [-1, 1, -1, -1, -1, -1] //<입출력 예시 맞나 ?-1,1아닌가
+        System.out.println("테스트 케이스 2 결과: " + Arrays.toString(result2)); // 예상 출력: [-1, 1, -1, -1, -1, -1]
     }
 
     public static int[] solution(String s) {
-        //이게 앞이랑 뒤의 차이가 뭐인거지 <<<<<<<
-        //뭔가 계수배열로 26개의 배열 만들어서 할수있을것같은데
-//        int k=26;
-        int k=200; //몇갱지모르겟지만
-        int[] arr=new int[k];
+        int size='z'-'a';
+        int N=s.length();
+        int[] count=new int[size];
         int[] result=new int[s.length()];
-        for (int i = 0; i <k ; i++) {
-            arr[i]=-1;
-
+        for (int i = 0; i <count.length ; i++) {
+            count[i]=-1;
         }
 
-
-
-        for (int i = 0; i < s.length(); i++) {
-
+        for (int i = N-1; i >=0; i--) {
             char c=s.charAt(i);
+            int index=c-'a';
 
-            if(arr[c]==-1){
-                arr[c]=i;
+            System.out.println(String.format("현재의 (문자,i,count의 index):(%c,%d,%d)",c,i,index));
+
+            if(count[index]==-1){
                 result[i]=-1;
+                count[index]=i;
             }
             else{
-//                arr[c]=i;순서문제 ?
-                result[i]=i-arr[c];
-                arr[c]=i;
+                result[i]=count[index]-i;
+                count[index]=i;
             }
+
         }
         return result;
-
-
-
     }
 }
